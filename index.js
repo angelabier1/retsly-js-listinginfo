2121900ad5570dd11f7b2c401fc35140ed9ae315
Luke Vivier <luke@vivier.ca>
2013-10-07 15:35:53 -0700

    component refactor

--- index.js
+++ index.js
@@ -2,51 +2,41 @@
  * Retsly ListingInfo Component
  * Requires Retsly SDK (Full hosted SDK including _, $, BB)
  */
+module.exports = exports = function(retsly){
 
-if(typeof Retsly !== 'undefined') {
+  var Component = {};
+  Component.Basic = Backbone.View.extend({
+    index: 0,
+    className: 'retsly-component retsly-js-listinginfo span12 row-fluid',
+    initialize: function(options) {
 
-  Retsly.Views.ListingInfo = module.exports = exports = (function(){
+      if(!options || typeof options.mls_id === "undefined")
+        throw new Error('Retsly.Views.ListingInfo requires a mls_id: `{mls_id: mls.id}`');
 
-    var Component = {};
-    Component.Basic = Backbone.View.extend({
-      index: 0,
-      className: 'retsly-component retsly-js-listinginfo span12 row-fluid',
-      initialize: function(options) {
+      if(!options || typeof options.listing_id === "undefined")
+        throw new Error('Retsly.Views.ListingInfo requires a listing_id: `{listing_id: listing.id}`');
 
-        if(!options || typeof options.mls_id === "undefined")
-          throw new Error('Retsly.Views.ListingInfo requires a mls_id: `{mls_id: mls.id}`');
+      if(typeof options == "undefined" || !options.target)
+        throw new Error('Retsly.Views.ListingInfo is a subview and must have a target: `{target:this}`');
 
-        if(!options || typeof options.listing_id === "undefined")
-          throw new Error('Retsly.Views.ListingInfo requires a listing_id: `{listing_id: listing.id}`');
+      this.options = _.extend({ mls_id: null, listing_id: null }, options);
+      options.target = (typeof options.target.$el !== "undefined") ? options.target.$el : $(options.target)
 
-        if(typeof options == "undefined" || !options.target)
-          throw new Error('Retsly.Views.ListingInfo is a subview and must have a target: `{target:this}`');
+      $(options.target).append(this.$el);
 
-        this.options = _.extend({ mls_id: null, listing_id: null }, options);
-        options.target = (typeof options.target.$el !== "undefined") ? options.target.$el : $(options.target)
+      var self = this;
+      new Retsly.Models.Listing({ _id: this.options.listing_id }, {
+        mls_id: this.options.mls_id,
+        complete: function(listing) { self.render(listing); }
+      }).fetch({ limit: 1 });
 
-        $(options.target).append(this.$el);
+    },
+    render: function(listing) {
+      var html = require('./templates/template');
+      var template = _.template(html);
+      this.$el.html( template( listing.toJSON() ));
+    }
+  });
 
-        var self = this;
-        new Retsly.Models.Listing({ _id: this.options.listing_id }, {
-          mls_id: this.options.mls_id,
-          complete: function(listing) { self.render(listing); }
-        }).fetch({ limit: 1 });
-
-      },
-      render: function(listing) {
-        var html = require('./templates/template');
-        var template = _.template(html);
-        this.$el.html( template( listing.toJSON() ));
-      }
-    });
-
-    return Component;
-
-  })();
-
-} else {
-  module.exports = exports = function(){
-    return; // NOOP.
-  };
+  return Component;
 }
